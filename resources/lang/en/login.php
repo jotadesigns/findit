fd027ccf68c407e93265f3e1cd7c3e0f9aa79f3a
Jorge Checa <jotacheca@gmail.com>
2016-12-15 20:47:21 +0100

    commit 2.1 - mas funcionalidades

---
+++ resources/lang/en/login.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | LOGIN
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'login-title' => 'Login',
+    'email' => 'Email',
+    'password' => 'Password',
+    'button-login' => 'Login',
+    'other' => 'Or',
+    'facebook' => 'Login with Facebook',
+    'beneficio1' => 'Access quickly and easily',
+    'beneficio2' => 'Customize your search results',
+    'forgotpassword' => 'Forgot ur password?',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | REGISTER
+    |--------------------------------------------------------------------------
+    */
+    'register-title' => 'Register',
+    'name' => 'Name',
+    'password_confirm' => 'Password confirm',
+    'button-register' => 'Register',
+
+
+];
---
+++ resources/lang/es/login.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | LOGIN
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'login-title' => 'Inicia sesión',
+    'email' => 'Email',
+    'password' => 'Contraseña',
+    'button-login' => 'INGRESAR',
+    'other' => 'O',
+    'facebook' => 'Inicia sesión con facebook',
+    'beneficio1' => 'Accede de forma rápida y sencilla',
+    'beneficio2' => 'Personaliza los resultados de busqueda',
+    'forgotpassword' => '¿Olvidaste la contraseña?',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | REGISTER
+    |--------------------------------------------------------------------------
+    */
+    'register-title' => 'Registro',
+    'name' => 'Nombre',
+    'password_confirm' => 'Confirma contraseña',
+    'button-register' => 'Registrarse',
+
+
+];
